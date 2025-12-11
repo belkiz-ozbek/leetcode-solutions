6605893c03db1661ed1b840b2b323fde7e250cfa
Belkız Özbek <74767722+belkiz-ozbek@users.noreply.github.com>
2025-12-11 14:00:29 +0300

    Time: 570 ms (35.89%), Space: 0B (100%) - LeetHub

---
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+SELECT ROUND(
+        COUNT(*) / (SELECT COUNT(DISTINCT player_id)
+                                    FROM Activity), 2) AS fraction
+FROM Activity a2
+INNER JOIN (
+        SELECT a.player_id, a.event_date
+        FROM Activity a
+        INNER JOIN (SELECT player_id, MIN(event_date) AS initial_date
+        FROM Activity
+        GROUP BY player_id
+    ) m ON a.player_id = m.player_id AND a.event_date = m.initial_date
+) d ON a2.player_id = d.player_id  
+    AND a2.event_date != d.event_date
+WHERE ABS(DATEDIFF(a2.event_date, d.event_date)) = 1
+
+
