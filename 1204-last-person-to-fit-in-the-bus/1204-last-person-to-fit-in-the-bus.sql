eb6829e2779f26e57015e558e37c663658d854fe
Belkız Özbek <74767722+belkiz-ozbek@users.noreply.github.com>
2025-12-16 11:10:56 +0300

    Time: 759 ms (62.23%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -4,14 +4,12 @@
 # turn sırasına göre eleman seç toplama ekle. toplam 1000 üzeri olunca dur bir önceki elemanı yazdır
 #turn'e göre sırala
 
-SELECT c.person_name
+SELECT person_name
 FROM (
     SELECT turn, person_name, SUM(weight) OVER (ORDER BY turn) AS cumulative_sum
     FROM Queue
 ) c
-JOIN Queue q 
-ON c.person_name = q.person_name
-WHERE c.cumulative_sum <= 1000
-ORDER BY c.turn DESC
+WHERE cumulative_sum <= 1000
+ORDER BY turn DESC
 LIMIT 1;
 
