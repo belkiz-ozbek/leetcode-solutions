c949a44cb29567de1fbdcb36bb209749257bdbd2
Belkız Özbek <74767722+belkiz-ozbek@users.noreply.github.com>
2025-12-17 09:50:36 +0300

    Time: 664 ms (12.04%), Space: 0B (100%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,20 +1,28 @@
 # Write your MySQL query statement below
 
-#customer_id'ler kaç defa tekrar ediyor 
+#ANOTHER SOLUTION
 SELECT customer_id
-FROM (
-    SELECT customer_id, COUNT(*) AS customer_id_tekrar_sayisi
-    FROM (
-        SELECT DISTINCT *
-        FROM Customer
-    ) c2
-    GROUP BY customer_id
-    ) c
-WHERE customer_id_tekrar_sayisi = 
-(
-    #kaç farklı product_id var
-    SELECT COUNT(*)
-    FROM Product
-) 
+FROM Customer
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(*) FROM Product);
 
+#customer_id'ler kaç defa tekrar ediyor 
+#SELECT customer_id
+#FROM (
+#    SELECT customer_id, COUNT(*) AS customer_id_tekrar_sayisi
+#    FROM (
+#        SELECT DISTINCT *
+#        FROM Customer
+#    ) c2
+#    GROUP BY customer_id
+#    ) c
+#WHERE customer_id_tekrar_sayisi = 
+#(
+#    #kaç farklı product_id var
+#    SELECT COUNT(*)
+#    FROM Product
+#) 
+#
+#
+#
 
