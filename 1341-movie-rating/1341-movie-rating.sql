d93c0326527c0a984e46c80bd1ba44fad3baf6e3
Belkız Özbek <74767722+belkiz-ozbek@users.noreply.github.com>
2025-12-17 12:34:53 +0300

    Time: 1197 ms (64.16%), Space: 0B (100%) - LeetHub

---
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -0,0 +1,53 @@
+# Write your MySQL query statement below
+
+WITH 
+Rating_Number AS(
+    SELECT name, COUNT(*) AS rating_number
+    FROM MovieRating mr
+    JOIN Users u
+    ON mr.user_id = u.user_id 
+    GROUP BY mr.user_id
+),
+Max_Rating_Number AS(
+    SELECT name, rating_number   
+    FROM Rating_Number
+    WHERE rating_number = (SELECT MAX(rating_number) FROM Rating_Number)
+),
+Avg_Rating AS(
+    SELECT title, AVG(rating) AS avg_rating
+    FROM MovieRating mr
+    INNER JOIN  Movies m
+    ON mr.movie_id = m.movie_id
+    WHERE created_at >= '2020-02-01' AND created_at < '2020-03-01'
+    GROUP BY m.title
+),
+Max_Avg_Rating AS(
+    SELECT title, avg_rating
+    FROM Avg_Rating
+    WHERE avg_rating = (
+        SELECT MAX(avg_rating)
+        FROM Avg_Rating
+    )
+)
+
+SELECT name AS results
+FROM Max_Rating_Number
+WHERE name = (
+    SELECT MIN(name) 
+    FROM Max_Rating_Number
+    )
+
+
+UNION ALL
+
+SELECT title AS results
+FROM Max_Avg_Rating
+WHERE title = (
+    SELECT MIN(title) 
+    FROM Max_Avg_Rating
+    )
+
+
+
+
+
