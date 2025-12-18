2daaf1f522e783c4929dc6912a747e53fa810192
Belkız Özbek <74767722+belkiz-ozbek@users.noreply.github.com>
2025-12-18 10:36:32 +0300

    Time: 327 ms (17.8%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,35 +1,66 @@
-WITH 
-IDs AS(
-    SELECT DISTINCT id
-    FROM (
-        SELECT DISTINCT requester_id AS id FROM RequestAccepted
-        UNION ALL
-        SELECT DISTINCT accepter_id AS id FROM RequestAccepted
-    ) i
+#ANOTHER SOLUTION
+WITH
+Friend_Relations AS(
+    SELECT 
+    requester_id AS id,
+    accepter_id AS friend_id
+    FROM RequestAccepted
     
+    UNION 
+    
+    SELECT 
+        accepter_id AS id,
+        requester_id AS friend_id
+    FROM RequestAccepted
 ),
-Friends_Number AS(
-    SELECT id, COUNT(*) AS friends_number
-    FROM IDs i
-    INNER JOIN RequestAccepted r
-    ON i.id = r.requester_id
-    GROUP BY id
-    UNION ALL
-    SELECT id, COUNT(*)
-    FROM IDs i
-    INNER JOIN RequestAccepted a
-    ON i.id = a.accepter_id
-    GROUP BY id
-),
-Friends_Number_With_Grouping AS(
-    SELECT id, SUM(friends_number) AS num
-    FROM Friends_Number
+Friend_Counts AS(
+    SELECT id, COUNT(*) friend_counts
+    FROM Friend_Relations
     GROUP BY id
 )
 
-SELECT id, num
-FROM Friends_Number_With_Grouping
-WHERE num = (SELECT MAX(num) FROM
-                (SELECT id, num
-                FROM Friends_Number_With_Grouping) fng
-            )
+SELECT id, friend_counts AS num
+FROM Friend_Counts 
+WHERE friend_counts = (SELECT MAX(friend_counts) FROM Friend_Counts)
+
+
+
+
+
+
+#WITH 
+#IDs AS(
+#    SELECT DISTINCT id
+#    FROM (
+#        SELECT DISTINCT requester_id AS id FROM RequestAccepted
+#        UNION ALL
+#        SELECT DISTINCT accepter_id AS id FROM RequestAccepted
+#    ) i
+#    
+#),
+#Friends_Number AS(
+#    SELECT id, COUNT(*) AS friends_number
+#    FROM IDs i
+#    INNER JOIN RequestAccepted r
+#    ON i.id = r.requester_id
+#    GROUP BY id
+#    UNION ALL
+#    SELECT id, COUNT(*)
+#    FROM IDs i
+#    INNER JOIN RequestAccepted a
+#    ON i.id = a.accepter_id
+#    GROUP BY id
+#),
+#Friends_Number_With_Grouping AS(
+#    SELECT id, SUM(friends_number) AS num
+#    FROM Friends_Number
+#    GROUP BY id
+#)
+#
+#SELECT id, num
+#FROM Friends_Number_With_Grouping
+#WHERE num = (SELECT MAX(num) FROM
+#                (SELECT id, num
+#                FROM Friends_Number_With_Grouping) fng
+#            )
+#
