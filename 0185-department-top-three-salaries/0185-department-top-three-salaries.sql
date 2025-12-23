f3ebde70746d4502b14b7ff694675c674b558126
Belkız Özbek <74767722+belkiz-ozbek@users.noreply.github.com>
2025-12-23 10:34:08 +0300

    Time: 805 ms (91.9%), Space: 0B (100%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,34 @@
+# Write your MySQL query statement below
+WITH Top_Thee_Unique_Salaries AS(
+    SELECT e.name AS employee_name, 
+           e.salary, 
+           e.departmentId, 
+           d.id, 
+           d.name AS department_name,
+           DENSE_RANK() OVER(PARTITION BY d.name ORDER BY salary DESC) 
+           AS denseRank
+    FROM Employee e
+    JOIN Department d
+    ON e.departmentId = d.id
+
+)
+
+SELECT department_name AS Department, 
+       employee_name AS Employee, 
+       salary AS Salary
+FROM Top_Thee_Unique_Salaries
+WHERE denseRank = 1 OR denseRank = 2 OR denseRank = 3
+
+
+
+#+----+-------+--------+--------------+---------------+
+#| id | name  | salary | departmentId | depatmentName |
+#+----+-------+--------+--------------+---------------+
+#| 1  | Joe   | 85000  | 1            | IT
+#| 2  | Henry | 80000  | 2            | Sales
+#| 3  | Sam   | 60000  | 2            | Sales
+#| 4  | Max   | 90000  | 1            | IT
+#| 5  | Janet | 69000  | 1            | IT
+#| 6  | Randy | 85000  | 1            | IT
+#| 7  | Will  | 70000  | 1            | IT
+#+----+-------+--------+--------------+
